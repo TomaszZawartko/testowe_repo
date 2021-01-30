f3bbf2134065b0acf2a2c7575af285bbbe5a3773
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-30 22:16:04 +0100

    auth-10 waiting for calsses

--- src/main/java/pl/test/ServiceImpl.java
+++ src/main/java/pl/test/ServiceImpl.java
@@ -6,5 +6,6 @@ public class ServiceImpl implements Service{
         System.out.println("Hello from auth-7");
         System.out.println("Second hello from auth-7");
         System.out.println("I continue my work on auth-7");
+        System.out.println("Waiting for class from auth-8 & auth-9: ");
     }
 }
