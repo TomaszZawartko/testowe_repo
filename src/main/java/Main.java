9469f47c9e74a71b6fa25b14d0c833b18dd6068a
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-30 18:44:12 +0100

    second commit fo auth-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("bla");
         System.out.println("added from github");
         System.out.println("feature/auth-1");
+        System.out.println("second commit to feature/auth-1");
     }
 }
