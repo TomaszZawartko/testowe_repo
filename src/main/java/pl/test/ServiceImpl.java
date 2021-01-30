829ffa2852d54613934271ffebeaddf4d7fc2ede
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-30 21:46:25 +0100

    second commit to auth-7

--- src/main/java/pl/test/ServiceImpl.java
+++ src/main/java/pl/test/ServiceImpl.java
@@ -4,5 +4,6 @@ public class ServiceImpl implements Service{
     public void hello() {
         System.out.println("Hello from auth-5");
         System.out.println("Hello from auth-7");
+        System.out.println("Second hello from auth-7");
     }
 }
