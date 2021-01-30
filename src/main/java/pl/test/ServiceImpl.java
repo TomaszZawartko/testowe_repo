9390639d815cd1e05e15592de8efc5b1d881140b
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-30 21:49:00 +0100

    continue work on auth-7

--- src/main/java/pl/test/ServiceImpl.java
+++ src/main/java/pl/test/ServiceImpl.java
@@ -5,5 +5,6 @@ public class ServiceImpl implements Service{
         System.out.println("Hello from auth-5");
         System.out.println("Hello from auth-7");
         System.out.println("Second hello from auth-7");
+        System.out.println("I continue my work on auth-7");
     }
 }
