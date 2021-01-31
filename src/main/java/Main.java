c662cc263e20097fb270b1047d5449964313338f
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2021-01-31 14:49:11 +0100

    commit abc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("Please call fish here: " + Fish.say());
         System.out.println("Please call sth other here: ");
         System.out.println("xyz");
+        System.out.println("abc");
 
     }
 }
